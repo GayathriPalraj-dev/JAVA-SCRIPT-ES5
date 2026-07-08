a65b1b894278c60fae3407e4759deea455ee5eb7
Gayathri Palraj <gayathri.2842004@gmail.com>
2026-07-08 19:33:03 +0530

    add background color

--- SYNCHRONOUS AND ASYNCHRONOUS EXAMPLE/asynchronousoperations.js
+++ SYNCHRONOUS AND ASYNCHRONOUS EXAMPLE/asynchronousoperations.js
@@ -1,16 +1,34 @@
-function startTask(){
-    setInterval(start,1000)
+let i = 0;
+let timer;
+
+function startTask() {
+    timer = setInterval(start, 1000);
 }
-i= 0;
-function start(){
+
+function start() {
+
     let dd = new Date();
-     document.getElementById("output").innerHTML = i;
-    console.log(i)
-    document.getElementById("date").innerHTML = dd.getHours()+ ":"+dd.getMinutes()+":"+dd.getSeconds()
-    let datetime = dd.getHours()+ ":"+dd.getMinutes()+":"+dd.getSeconds()
-    console.log(datetime)
+
+    document.getElementById("output").innerHTML = i;
+    console.log(i);
+
+    let datetime =
+        dd.getHours() + ":" +
+        dd.getMinutes() + ":" +
+        dd.getSeconds();
+
+    document.getElementById("date").innerHTML = datetime;
+    console.log(datetime);
+
+    if (i % 2 == 0) {
+        document.getElementsByTagName("body")[0].style.backgroundColor = "pink";
+    } else {
+        document.getElementsByTagName("body")[0].style.backgroundColor = "green";
+    }
+
     i++;
 }
-function stop(){
 
+function stop() {
+    clearInterval(timer);
 }
