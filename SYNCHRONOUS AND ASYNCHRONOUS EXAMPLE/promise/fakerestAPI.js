ab0b6f3bb748f96df841f65685fb7f82f42a1e3f
Gayathri Palraj <gayathri.2842004@gmail.com>
2026-07-09 10:02:07 +0530

    fetch()

--- SYNCHRONOUS AND ASYNCHRONOUS EXAMPLE/promise/fakerestAPI.js
+++ SYNCHRONOUS AND ASYNCHRONOUS EXAMPLE/promise/fakerestAPI.js
@@ -1,4 +1,13 @@
-fetch('https://fakestoreapi.com/products/1')
+fetch('https://fakestoreapi.com/products/')
 .then(response=>response.json())
-.then(result=>console.log(result))
+.then(result=>{
+    result.forEach(data=>{
+        let div = document.createElement("div");
+        let textNode = document.createTextNode(data.title);
+        div.appendChild(textNode);
+        document.getElementsByTagName("body")[0].appendChild(div);
+
+    });
+})
+
 .catch(error=>console.log(error));
